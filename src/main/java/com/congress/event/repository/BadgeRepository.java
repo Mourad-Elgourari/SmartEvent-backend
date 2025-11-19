ed0757061f12b376287ba8b9585f8c03b1f0cd0a
MOURAD ELGOURARI <muradelgourarion15@gmail.com>
2025-11-19 15:12:22 +0100

    create project repositories

---
+++ src/main/java/com/congress/event/repository/BadgeRepository.java
@@ -0,0 +1,9 @@
+package com.congress.event.repository;
+
+import com.congress.event.model.Badge;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BadgeRepository extends JpaRepository<Badge, Long> {
+}
---
+++ src/main/java/com/congress/event/repository/GuestRepository.java
@@ -0,0 +1,9 @@
+package com.congress.event.repository;
+
+import com.congress.event.model.Guest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends JpaRepository<Guest, Long> {
+}
